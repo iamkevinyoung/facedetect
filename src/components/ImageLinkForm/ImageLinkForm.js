fdec1a02b582fe3ab74cb8b40b753f80481a038d
iamkevinyoung <iamkevinyoung@gmail.com>
2018-08-17 22:48:07 +0100

    adding files.

---
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './ImageLinkForm.css';
+
+const ImageLinkForm = ({onInputChange, onPictureSubmit}) => {
+	return (
+		<div>
+			<p className = 'f3'>
+				{'Let me find a face in any picture!'}
+			</p>
+			<div className = 'center'>
+				<div className = 'form center pa4 br3 shadow-5'>
+				<input className = 'f4 pa2 w-70 center' type = 'tex' onChange = {onInputChange}/>
+				<button className = 'w-30 grow f4 link ph3 pv2  div black bg-yellow'
+					onClick = {onPictureSubmit}>Detect!</button>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default ImageLinkForm;
